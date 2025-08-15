c629498061d417ef8eb8197c2967a0098990eadf
Beny Adi Nugraha <benyadin17@gmail.com>
2025-08-16 00:51:45 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
-
+import './globals.css';
 import 'swiper/css';
-import 'swiper/css/effect-cards';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return <html lang="en"><body>{children}</body></html>;
